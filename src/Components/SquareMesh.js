204e5675c950b829fa5d45d1a0b12452d7533660
Pro-Developer01 <yadavshashank70@gmail.com>
2022-09-11 01:58:00 +0530

    Squaremesh.js Componenet created and its css

---
+++ src/Components/SquareMesh.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Components/SquarMesh.css'
+
+export default function SquareMesh() {
+  return (
+    <div>
+      <span className='square'>x</span>
+    </div>
+  )
+}
