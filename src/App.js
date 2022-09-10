cbaa9e0f0de2112e8938b93a630bc5be47b78f4d
Pro-Developer01 <yadavshashank70@gmail.com>
2022-09-11 01:57:19 +0530

    Basic Setup and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SquareMesh from './Components/SquareMesh'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='app-header'>
+      <p className="app-header">heading-text</p>
+      <div className="row jc-center">
+      <SquareMesh/>
+      <SquareMesh/>
+      <SquareMesh/>
+      </div>      
+      <div className="row jc-center">
+      <SquareMesh/>
+      <SquareMesh/>
+      <SquareMesh/>
+      </div>      
+      <div className="row jc-center">
+      <SquareMesh/>
+      <SquareMesh/>
+      <SquareMesh/>
+      </div>      
+      </div>      
+      
+  )
 }
-
-export default App;
