605689c9151fb30120f24758ae9ef179212a58cd
Pro-Developer01 <yadavshashank70@gmail.com>
2022-09-15 02:18:34 +0530

    Login completetd

--- src/App.js
+++ src/App.js
@@ -1,35 +1,78 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SquareMesh from './Components/SquareMesh'
 
-const initialState=[""," "," "," "," "," "," "," "," "];
+const initialState=["","","","","","","","",""];
 
 export default function App() {
-  const [turn,setTurn]=useState("X");
-  const [gameState,setGamestate]=useState(initialState);
-const clickHandler=(e)=>{
-console.log("check",e);
-}
+  const [Xturn,setTurn]=useState(true);
+  const [pause,setPause]=useState(false);
+  const [Message,setMessage]=useState("Player X's Turn");
+  const [gameState,setGamestate]=useState([initialState]);
+
+  const clickHandler=(i)=>{
+    if (!pause){
+    var strings=Array.from(gameState);
+    strings[i]=Xturn?"X":"0";
+    setGamestate(strings);
+    console.log("gamestate",gameState);
+    setTurn(!Xturn);
+    setMessage(Xturn?"Player 0's Turn":"Player X's Turn");}
+  }
+
+  const clearFunc=()=>{
+    setGamestate(initialState); console.log("gameState",gameState);
+    setPause(false);
+  }
+
+  const CheckWin=()=>{
+    let win=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+
+    win.forEach((e)=>{
+      const [a,b,c]=e;
+      if(gameState[a] && gameState[a]===gameState[b] && gameState[b]===gameState[c])
+      {
+        setMessage("Player "+gameState[a]+" Wins !");
+        setPause(true);
+
+      }
+    })
+  }
+useEffect(()=>{
+  CheckWin();
+},[Xturn])
   return (
     <div className='app-header'>
-      <p className="heading-text ">heading-text</p>
+      <p className="heading-text ">{Message}</p>
+      <div >
+
       <div className="row jc-center">
-        <SquareMesh className="b-bottom-right" initialState={gameState[0]} function={()=>{gameState[0]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
-        <SquareMesh className="b-bottom-right" initialState={gameState[1]}  function={()=>{gameState[1]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
-        <SquareMesh className="b-bottom " initialState={gameState[2]} function={()=>{gameState[2]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
+        <SquareMesh className="b-bottom-right" initialState={gameState[0]} function={()=>{clickHandler(0)}}/>
+        <SquareMesh className="b-bottom-right" initialState={gameState[1]}  function={()=>{clickHandler(1)}}/>
+        <SquareMesh className="b-bottom " initialState={gameState[2]} function={()=>{clickHandler(2)}}/>
       </div>
       <div className="row jc-center">
-        <SquareMesh className="b-bottom-right " initialState={gameState[3]} function={()=>{gameState[3]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
-        <SquareMesh className="b-bottom-right " initialState={gameState[4]} function={()=>{gameState[4]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
-        <SquareMesh className="b-bottom" initialState={gameState[5]} function={()=>{gameState[5]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
+        <SquareMesh className="b-bottom-right " initialState={gameState[3]} function={()=>{clickHandler(3)}}/>
+        <SquareMesh className="b-bottom-right " initialState={gameState[4]} function={()=>{clickHandler(4)}}/>
+        <SquareMesh className="b-bottom" initialState={gameState[5]} function={()=>{clickHandler(5)}}/>
       </div>
       <div className="row jc-center">
-        <SquareMesh className="b-right " initialState={gameState[6]} function={()=>{gameState[6]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
-        <SquareMesh className="b-right " initialState={gameState[7]} function={()=>{gameState[7]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
-        <SquareMesh initialState={gameState[8]} function={()=>{gameState[8]=`${turn}`; setTurn(turn==="X"? "0":"X")}}/>
+        <SquareMesh className="b-right " initialState={gameState[6]} function={()=>{clickHandler(6)}}/>
+        <SquareMesh className="b-right " initialState={gameState[7]} function={()=>{clickHandler(7)}}/>
+        <SquareMesh initialState={gameState[8]} function={()=>{clickHandler(8)}}/>
+      </div>
       </div>
 
       <div className="button">
-        <button className='clear-button' onClick={()=>{setGamestate([initialState]); console.log("gameState",gameState);}}>Clear Board</button>
+        <button className='clear-button' onClick={clearFunc}>Clear Board</button>
       </div>
     </div>
 
--- src/Components/SquareMesh.js
+++ src/Components/SquareMesh.js
@@ -4,7 +4,7 @@ import '../Components/SquarMesh.css'
 export default function SquareMesh(props) {
     const classes=(props.className?`${props.className} square`:'square');
   return (
-    <div >
+    <div id="boxes" >
       <span className={classes} onClick={props.function}>{props.initialState}</span>
     </div>
   )
