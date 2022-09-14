9eb9394076773226e273618253f92ffb65742460
Pro-Developer01 <yadavshashank70@gmail.com>
2022-09-15 02:20:08 +0530

    Board Alingment Fixed

--- src/Components/SquareMesh.js
+++ src/Components/SquareMesh.js
@@ -5,7 +5,7 @@ export default function SquareMesh(props) {
     const classes=(props.className?`${props.className} square`:'square');
   return (
     <div id="boxes" >
-      <span className={classes} onClick={props.function}>{props.initialState}</span>
+      <p className={classes} onClick={props.function}>{props.initialState}</p>
     </div>
   )
 }
